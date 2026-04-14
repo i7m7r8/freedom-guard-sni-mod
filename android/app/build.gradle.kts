163104a9193f1a2096b92a2a34dd20831d061558
User <user@example.com>
2026-04-15 05:36:04 +0600

    Remove Google Services plugin to fix CI build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,7 @@ import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    id("com.google.gms.google-services")
+    
     id("dev.flutter.flutter-gradle-plugin")
 }
 
